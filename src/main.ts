62bb9b885f2fd8b1bbb9987e836ffe5dc232a9ec
Menilek Techane <menilek25@hotmail.co.uk>
2022-09-18 22:07:06 +0100

    Add WaveUI

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
+import WaveUI from 'wave-ui'
+import 'wave-ui/dist/wave-ui.css'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+new WaveUI(app);
+
+app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from "path"
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
+export default defineConfig(({mode}) => {
+  return {
+    build: {
+      target: 'esnext'
+    },
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src'),
+        '~': resolve(__dirname, 'node_modules')
+      }
+    },
+    plugins: [vue()]
+  }
 })
